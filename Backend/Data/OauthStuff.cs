a93ae7de04da3b35fe9553f1140b44392c920786
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-23 12:30:05 +0100

    oauth class

---
+++ Backend/Data/OauthStuff.cs
@@ -0,0 +1,7 @@
+namespace Backend.Data;
+
+public class OauthStuff
+{
+    public string ClientID = "1061925551073-28j75e6a29ukrfosq7otehkbe05auqj3.apps.googleusercontent.com";
+    public string ClientSecret = "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";
+}
