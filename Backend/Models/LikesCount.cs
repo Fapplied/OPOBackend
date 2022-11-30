29ca97f12691f075122abc16c68db0345391ffc9
Rinaldo Wouterson <rinaldo@Rinaldos-MacBook-Air.local>
2022-11-30 10:31:46 +0100

    switching

---
+++ Backend/Models/LikesCount.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace Backend.Models;
+
+public class LikesCount
+{
+   
+public int ProLikes {get; set;} 
+
+public int ConLikes {get; set;} 
+
+}
